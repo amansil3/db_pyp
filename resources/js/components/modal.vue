482100ebeaddbcf9786050c11ec3dc0623259c5d
amansil3 <amansil3@rosario.gov.ar>
2019-11-19 23:13:17 -0300

    second commit

--- resources/js/components/modal.vue
+++ resources/js/components/modal.vue
@@ -1,16 +1,57 @@
 <template>
   <v-row justify="center">
-    <v-dialog v-model="abrir" persistent max-width="290">
+    <v-dialog v-model="abrir" persistent max-width="600px">
+
       <template v-slot:activator="{ on }">
-        <v-btn color="primary" dark v-on="on">Abrir el Modal</v-btn>
+        <v-btn color="primary" dark v-on="on">Cargar un nuevo ejemplar</v-btn>
       </template>
+
       <v-card>
-        <v-card-title class="headline">Use Google's location service?</v-card-title>
-        <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+
+        <v-card-title>
+          <span class="headline">Nuevo ejemplar</span>
+        </v-card-title>
+
+        <v-card-text>
+          <v-container>
+
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                 label="Nombre vulgar" 
+                 required>
+                </v-text-field>
+              </v-col>
+
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field 
+                label="Nombre científico" 
+                hint="example of helper text only on focus">
+                </v-text-field>
+              </v-col>
+            </v-row>
+            
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  label="Cantidad"
+                  type="number"
+                  min="0"
+                  hint="No puede ser menor a 0"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+
+          </v-container>
+          <!-- <small>*indicates required field</small> -->
+        </v-card-text>
+
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="green darken-1" text @click="abrir = false">Disagree</v-btn>
-          <v-btn color="green darken-1" text @click="abrir = false">Agree</v-btn>
+          <v-btn color="error" @click="abrir = false">Cerrar</v-btn>
+          <v-btn color="success" @click="abrir = false">Cargar</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -22,16 +63,6 @@
     props:[
       'abrir'
     ],
-    /*
-    watch:
-    {
-      abrir:{
-        handler(){
-
-        }
-      }
-    },
-    */
     data () {
       return {
         dialog: false,
