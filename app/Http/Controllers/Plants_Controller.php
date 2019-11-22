92c7048b8ad6ff6622ef96c0fb05e6615ac087bf
amansil3 <andres.mansilla.02@gmail.com>
2019-11-22 07:27:51 -0300

    asd

--- app/Http/Controllers/Plants_Controller.php
+++ app/Http/Controllers/Plants_Controller.php
@@ -14,20 +14,49 @@ class Plants_Controller extends Controller
         return view('plants.plants', ['plants' => $query]);
     }
     public function ReceptForm(Request $request){
-
+        /*
         \DB::table('plants')->insert([
 	        'nombre' => $request['nombreVulgar'],
 	        'cientifico' => $request['nombreCientifico'],
 	        'tipo' => $request['tipo'],
 	        'cantidad' => $request['cantidad'],
 	    ]);
+        */
+
+        $planta = new Plantas;
+
+            $planta->nombre = $request->nombreVulgar;
+            $planta->cientifico = $request->nombreCientifico;
+            $planta->tipo = $request->tipo;
+            $planta->cantidad = $request->cantidad;
+
+        $planta->save();
 
         return view('subindex');
     }
 
+    public function Update(Request $request){
+        /*\DB::table('plants')
+            ->where('id', $request['$id'])
+            ->update(['estado' => false]);*/
+
+        $plantaDelete = Plantas::find($request['id'])
+            $plantaDelete->nombre = $request->nombreVulgar;
+            $plantaDelete->cientifico = $request->nombreCientifico;
+            $plantaDelete->tipo = $request->tipo;
+            $plantaDelete->cantidad = $request->cantidad;
+        $plantaDelete->save();
+
+    }
+
     public function Delete(Request $request){
-    	\DB::table('plants')
+    	/*\DB::table('plants')
 			->where('id', $request['$id'])
-			->update(['estado' => false]);
+			->update(['estado' => false]);*/
+
+        $plantaDelete = Plantas::find($request['id'])
+            $plantaDelete->estado = false;
+        $plantaDelete->save();
+
     }
 }
--- public/js/app.js
+++ public/js/app.js
@@ -88569,8 +88569,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\db_pyp\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\db_pyp\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\XamPPp\htdocs\db_pyp\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\XamPPp\htdocs\db_pyp\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
